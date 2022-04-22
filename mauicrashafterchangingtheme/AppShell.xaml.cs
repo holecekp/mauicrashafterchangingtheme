143862ace423861f80cd30661c0b68a03738cf5a
holecekp <holecekp@holecekp.com>
2022-04-22 20:59:49 +0200

    Add project files.

---
+++ mauicrashafterchangingtheme/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace mauicrashafterchangingtheme;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ mauicrashafterchangingtheme/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace mauicrashafterchangingtheme;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
